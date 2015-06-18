2a1c1b1ab9a61e91091d78da5f356df75cc34785
Dawen Liang <dl2771@columbia.edu>
2015-06-17 23:34:45 -0700

    initial commit

---
+++ code/batched_inv_joblib.py
@@ -0,0 +1,68 @@
+import numpy as np
+
+from joblib import Parallel, delayed
+
+
+def get_row(S, i):
+    lo, hi = S.indptr[i], S.indptr[i + 1]
+    return S.data[lo:hi], S.indices[lo:hi]
+
+
+def solve_sequential(As, Bs):
+    X_stack = np.empty_like(As, dtype=As.dtype)
+
+    for k in xrange(As.shape[0]):
+        X_stack[k] = np.linalg.solve(Bs[k], As[k])
+
+    return X_stack
+
+
+def solve_batch(b, S, Y, WX, YTYpR, batch_size, m, f, dtype):
+    lo = b * batch_size
+    hi = min((b + 1) * batch_size, m)
+    current_batch_size = hi - lo
+
+    A_stack = np.empty((current_batch_size, f), dtype=dtype)
+    B_stack = np.empty((current_batch_size, f, f), dtype=dtype)
+
+    for ib, k in enumerate(xrange(lo, hi)):
+        s_u, i_u = get_row(S, k)
+
+        Y_u = Y[i_u]  # exploit sparsity
+        A = (s_u + 1).dot(Y_u)
+
+        if WX is not None:
+            A += WX[:, ib]
+
+        YTSY = np.dot(Y_u.T, (Y_u * s_u[:, None]))
+        B = YTSY + YTYpR
+
+        A_stack[ib] = A
+        B_stack[ib] = B
+
+    X_stack = solve_sequential(A_stack, B_stack)
+    return X_stack
+
+
+def recompute_factors_batched_mp(Y, S, lambda_reg, W=None, X=None,
+                                 dtype='float32', batch_size=1,
+                                 n_jobs=4):
+    m = S.shape[0]  # m = number of users
+    f = Y.shape[1]  # f = number of factors
+
+    YTY = np.dot(Y.T, Y)  # precompute this
+    YTYpR = YTY + lambda_reg * np.eye(f)
+    if W is not None:
+        WX = lambda_reg * W.T.dot(X.T)
+    else:
+        WX = None
+    X_new = np.zeros((m, f), dtype=dtype)
+
+    num_batches = int(np.ceil(m / float(batch_size)))
+
+    res = Parallel(n_jobs=n_jobs)(delayed(solve_batch)(b, S, Y, WX, YTYpR,
+                                                       batch_size, m, f, dtype)
+                                  for b in xrange(num_batches))
+    X_new = np.concatenate(res, axis=0)
+
+    return X_new
---
+++ code/content_wmf.py
@@ -0,0 +1,126 @@
+import sys
+import time
+
+import numpy as np
+from scipy import weave
+
+import batched_inv_mp
+
+
+def linear_surplus_confidence_matrix(B, alpha):
+    # To construct the surplus confidence matrix, we need to operate only on
+    # the nonzero elements.
+    # This is not possible: S = alpha * B
+    S = B.copy()
+    S.data = alpha * S.data
+    return S
+
+
+def log_surplus_confidence_matrix(B, alpha, epsilon):
+    # To construct the surplus confidence matrix, we need to operate only on
+    # the nonzero elements.
+    # This is not possible: S = alpha * np.log(1 + B / epsilon)
+    S = B.copy()
+    S.data = alpha * np.log(1 + S.data / epsilon)
+    return S
+
+
+def factorize(S, num_factors, X=None, vad=None, num_iters=10, init_std=0.01,
+              lambda_U_reg=1e-5, lambda_V_reg=1e-5, lambda_W_reg=1e-5,
+              dtype='float32', random_state=None, verbose=False,
+              recompute_factors=batched_inv_mp.recompute_factors_batched_mp,
+              *args, **kwargs):
+
+    num_users, num_items = S.shape
+    if X is not None:
+        assert X.shape[0] == num_items
+        assert np.all(X[:, -1] == 1)
+
+    if verbose:
+        print "precompute S^T and X^TX (if necessary)"
+        start_time = time.time()
+
+    ST = S.T.tocsr()
+    if X is not None:
+        n_feats = X.shape[1]
+        R = np.eye(n_feats)
+        R[n_feats - 1, n_feats - 1] = 0
+        XTXpR = X.T.dot(X) + lambda_W_reg * R
+
+    if verbose:
+        print "  took %.3f seconds" % (time.time() - start_time)
+        print "run ALS algorithm"
+        start_time = time.time()
+
+    if type(random_state) is int:
+        np.random.seed(random_state)
+    elif random_state is not None:
+        np.random.setstate(random_state)
+
+    U = np.random.randn(num_users, num_factors).astype(dtype) * init_std
+    V = None  # no need to initialize V, it will be overwritten anyway
+    if X is not None:
+        W = np.random.randn(X.shape[1], num_factors).astype(dtype) * init_std
+    else:
+        W = None
+
+    for i in xrange(num_iters):
+        if verbose:
+            print("Iteration %d:" % i)
+            start_t = _write_and_time('\tUpdating item factors...')
+        V = recompute_factors(U, ST, lambda_V_reg, W=W, X=X, dtype=dtype,
+                              *args, **kwargs)
+        if verbose:
+            print('\r\tUpdating item factors: time=%.2f'
+                  % (time.time() - start_t))
+
+            start_t = _write_and_time('\tUpdating user factors...')
+        U = recompute_factors(V, S, lambda_U_reg, dtype=dtype, *args, **kwargs)
+
+        if verbose:
+            print('\r\tUpdating user factors: time=%.2f'
+                  % (time.time() - start_t))
+            pred_ll = _pred_loglikeli(U, V, dtype, **vad)
+            print("\tPred likeli: %.5f" % pred_ll)
+            sys.stdout.flush()
+
+        if X is not None:
+            if verbose:
+                start_t = _write_and_time(
+                    '\tUpdating projection matrix...')
+            W = np.linalg.solve(XTXpR, X.T.dot(V))
+
+            if verbose:
+                print('\r\tUpdating projection matrix: time=%.2f'
+                      % (time.time() - start_t))
+    return U, V, W
+
+
+def _pred_loglikeli(U, V, dtype, X_new=None, rows_new=None, cols_new=None):
+    X_pred = _inner(U, V, rows_new, cols_new, dtype)
+    pred_ll = np.mean((X_new - X_pred)**2)
+    return pred_ll
+
+
+def _write_and_time(s):
+    sys.stdout.write(s)
+    sys.stdout.flush()
+    return time.time()
+
+
+def _inner(U, V, rows, cols, dtype):
+    n_ratings = rows.size
+    n_components = U.shape[1]
+    assert V.shape[1] == n_components
+    data = np.empty(n_ratings, dtype=dtype)
+    code = r"""
+    for (int i = 0; i < n_ratings; i++) {
+       data[i] = 0.0;
+       for (int j = 0; j < n_components; j++) {
+           data[i] += U[rows[i] * n_components + j] * V[cols[i] * n_components + j];
+       }
+    }
+    """
+    weave.inline(code, ['data', 'U', 'V', 'rows', 'cols', 'n_ratings',
+                        'n_components'])
+    return data
